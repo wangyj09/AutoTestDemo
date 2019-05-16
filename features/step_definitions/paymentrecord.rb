708a6724683d26276257ef618b8f08c9a77af24e
WangYJ <wangyj@WangYJdeMacBook-Pro.local>
2019-05-16 18:46:16 +0800

    err

--- features/step_definitions/paymentrecord.rb
+++ features/step_definitions/paymentrecord.rb
@@ -10,7 +10,8 @@ end
     expect(actual).to eq(false)
 end
 当("点击为他人交费记录") do
-    driver.find_element(:accessibility_id, "为他人交费记录").click
+    #driver.find_element(:accessibility_id, "为他人交费记录").click
+    driver.find_element(:accessibility_id, "123").click
 end
 
 那么("进入为他人交费记录页面") do
